c04dbe1e52508e5dc962bc86ea6f9dc12fd62cf7
David Rosson <david@rosson.com.au>
2015-10-20 00:20:21 -0700

    added image reading

--- client/js/mosaic.js
+++ client/js/mosaic.js
@@ -1,6 +1,6 @@
 function selected(event) {
-  var img = event.target.files[0];
-  console.log(img);
+  var ref = event.target.files[0];
+  draw(ref);
 }
 
 function dragging(event) {
@@ -12,15 +12,33 @@ function dragging(event) {
 function dropped(event) {
   event.stopPropagation();
   event.preventDefault();
-  var img = event.dataTransfer.files[0];
-  console.log(img);
+  var ref = event.dataTransfer.files[0];
+  draw(ref);
 }
 
 document.getElementById('file')
-  .addEventListener('change', selected, false);
+  .addEventListener('change', selected);
 
 document.getElementById('zone')
-  .addEventListener('dragover', dragging, false);
+  .addEventListener('dragover', dragging);
+
 document.getElementById('zone')
-  .addEventListener('drop', dropped, false);
-  
+  .addEventListener('drop', dropped);
+
+function draw(source) {
+  var image = new Image();
+  var reader = new FileReader();
+  var rendered = document.createElement('canvas').getContext('2d');
+  reader.onload = function(event) {
+    image.src = event.target.result;
+    rendered.drawImage(image, 0, 0);
+    analyze(rendered);
+  };
+  reader.readAsDataURL(source);
+}
+
+function analyze(image) {
+  // determine image size
+
+  // sample colors
+}
