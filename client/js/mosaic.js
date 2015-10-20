1e42c61f3fc2eecf6dab757775ae2cbdf7518500
David Rosson <david@rosson.com.au>
2015-10-19 22:30:41 -0700

    switched to native drag-n-drop

--- client/js/mosaic.js
+++ client/js/mosaic.js
@@ -1,6 +1,26 @@
+function selected(event) {
+  var img = event.target.files[0];
+  console.log(img);
+}
 
+function dragging(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'copy';
+}
 
-// Test availablity of `Promise`.
-// 
-// Prompt: 
-// Get your Chrome: https://www.google.com/chrome/browser/desktop/
+function dropped(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  var img = event.dataTransfer.files[0];
+  console.log(img);
+}
+
+document.getElementById('file')
+  .addEventListener('change', selected, false);
+
+document.getElementById('zone')
+  .addEventListener('dragover', dragging, false);
+document.getElementById('zone')
+  .addEventListener('drop', dropped, false);
+  
