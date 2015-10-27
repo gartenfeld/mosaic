e53ea2fce66ed55669b40c45eb45d32c72f6e849
David Rosson <david@rosson.com.au>
2015-10-27 08:30:44 -0700

    working in chrome

--- client/js/factory.js
+++ client/js/factory.js
@@ -1,21 +1,107 @@
 importScripts('settings.js');
 
-var cache = {};
+// COLOR_DEPTH = 16;
+// SAMPLE_RATE = 8;
+var IMAGE_WIDTH = 0;
 
-var sampleAverageColor = function(data) {
-
-  // read rgb values from random location in the array
-
-  // var x = 0, y = 0;
-  // var pixel = image.getImageData(x, y, 1, 1).data;
-  // console.log(pixel);
-
+var hexify = function(num) {
+  return ('00' + num.toString(16)).substr(-2);
 };
 
 var reduceColor = function(r, g, b) {
-  var hex;
-  return hex;
+  r -= (r % COLOR_DEPTH);
+  g -= (g % COLOR_DEPTH);
+  b -= (b % COLOR_DEPTH);
+  return hexify(r) + hexify(g) + hexify(b);
 };
 
-// COLOR_DEPTH = 16;
-// SAMPLE_RATE = 8;
+// var encodeTile = function(blob) {
+//   var buffer = new Uint8Array(blob),
+//       i = buffer.length,
+//       binary = new Array(i);
+//   while (i--) {
+//     binary[i] = String.fromCharCode(buffer[i]);
+//   } 
+//   return btoa(binary.join(''));
+// };
+
+var downloadTile = function(hex, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'blob';
+  var reader = new FileReader();
+  xhr.onload = function() {
+    if (this.status === 200) {
+      reader.onloadend = function() {             
+        callback(reader.result);
+      };
+      reader.readAsDataURL(this.response); 
+    }
+  };
+  xhr.open('GET', '../color/' + hex, true);
+  xhr.send();
+};
+
+var cache = {};
+
+var retrieveTile = function(hex, callback) {
+  if (cache[hex]) {
+    callback(cache[hex]);
+  } else {
+    downloadTile(hex, function(tile) {
+      callback(tile);
+      cache[hex] = tile;
+    });
+  }
+};
+
+var reduceRowData = function(flat, callback) {
+
+  IMAGE_WIDTH = flat.length / (4 * TILE_HEIGHT);
+
+  var sampleAverageColor = function(offset) {
+    var cursor, x, y, 
+        rSum = 0, gSum = 0, bSum = 0, r, g, b;
+    for (var s = 0; s < SAMPLE_RATE; s++) {
+      x = Math.floor(Math.random() * TILE_WIDTH);
+      y = Math.floor(Math.random() * TILE_HEIGHT);
+      cursor = (y * IMAGE_WIDTH + offset + x) * 4;
+      rSum += flat[cursor];
+      gSum += flat[cursor + 1];
+      bSum += flat[cursor + 2];
+    }
+    r = Math.floor(rSum / SAMPLE_RATE);
+    g = Math.floor(gSum / SAMPLE_RATE);
+    b = Math.floor(bSum / SAMPLE_RATE);
+    return reduceColor(r, g, b);
+  };
+
+  var tiles = [],
+      stack = 0;
+
+  var buildTileElement = function(offset) {
+    retrieveTile(sampleAverageColor(offset), function(base64) {
+      tiles[offset / TILE_WIDTH] = base64;
+      if (--stack === 0) {
+        buildRowHTML();
+      }
+    });
+  };
+
+  var html = '';
+
+  var buildRowHTML = function() {
+    tiles.forEach(function(base64) {
+      html += '<img src="' + base64 + '" />';
+    });
+    callback(html);
+  };
+
+  for (var i = 0; i < IMAGE_WIDTH; i += TILE_WIDTH) {
+    stack++;
+    buildTileElement(i);
+  }
+
+};
+
+
+
--- client/js/worker.js
+++ client/js/worker.js
@@ -1,18 +1,8 @@
 importScripts('factory.js');
 
 onmessage = function (e) {
-  var row = e.data.row;
-  var IMAGE_WIDTH = row.length / (4 * TILE_HEIGHT);
-
-  // available from 'shared'
-  // someFunction();
-  var html = '';
-  for (var i = 0; i < IMAGE_WIDTH; i += TILE_WIDTH) {
-    html += '<img src="color/cccc00" />';
-  }
-  var payload = { results: html, index: e.data.index };
-  
-  setTimeout(function() {
-    postMessage(payload);
-  }, 0);
+  reduceRowData(e.data.row, function(html) {
+    postMessage({ results: html, index: e.data.index });
+    close();
+  });
 };
